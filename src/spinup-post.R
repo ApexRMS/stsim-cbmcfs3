cd0402f2c48712683399e61df84a2bb3850af867
vlucet <VLucet@users.noreply.github.com>
2020-09-18 18:13:00 +0000

    first lines for post processing - loading data

--- src/spinup-post.R
+++ src/spinup-post.R
@@ -1,1 +1,30 @@
+########################################################
+## A223 - LandCarbon - Spinnup post-processing script ##
+## Prepared bt VL sept 2020                           ##
+########################################################
+
+library(rsyncrosim)
+library(tidyverse)
+
+myLibrary <- ssimLibrary()
+myProject <- project(myLibrary, 1)
+myScenario <- scenario()
+
+# Source helper functions
+pkg_dir <- (Sys.getenv("ssim_package_directory"))
+source(file.path(pkg_dir, "stsimcbmcfs3_helpers.R"))
+
+# (1) Extract source and destination datasheets ---------------------------
+
+spinup <- datasheet(myScenario, "stsimcbmcfs3_Spinup") %>%
+  mutate_if(is.factor, as.character)
+
+output_stocks <- datasheet(myScenario, "stsimsf_OutputStock") %>% 
+  mutate_if(is.factor, as.character) %>% 
+  filter()
+
+state_attributes <- datasheet(myScenario, "stsim_StateAttributeValue") %>% 
+  mutate_if(is.factor, as.character)
+
+
 
